9888fe3e6ad456163c9c9960d17637f94ad2b59e
eslaamadel <eslaamadelwahba@gmail.com>
2024-12-28 22:51:26 +0200

    edit placeholder text-size

--- src/components/ui/textarea.tsx
+++ src/components/ui/textarea.tsx
@@ -10,7 +10,7 @@ const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
     return (
       <textarea
         className={cn(
-          "flex min-h-[150px] w-full rounded-md border border-netu-600/50 bg-transparent px-3 py-2 text-p shadow-sm placeholder:text-netu-600 focus-visible:outline-none focus-visible:ring-1 focus-visible:border-accent-default disabled:cursor-not-allowed disabled:opacity-50 dark:border-slate-800 dark:placeholder:text-slate-400 dark:focus-visible:ring-slate-300",
+          "flex min-h-[150px] w-full rounded-md border border-netu-600/50 bg-transparent px-3 py-2 text-p shadow-sm mb:placeholder:text-[15px] tb:placeholder:text-[15px] placeholder:text-netu-600 focus-visible:outline-none focus-visible:ring-1 focus-visible:border-accent-default disabled:cursor-not-allowed disabled:opacity-50 dark:border-slate-800 dark:placeholder:text-slate-400 dark:focus-visible:ring-slate-300",
           className
         )}
         ref={ref}
