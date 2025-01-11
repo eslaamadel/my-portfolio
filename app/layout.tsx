2439785b5a41205a3d4ef012c3362af39edf0650
eslaamadel <eslaamadelwahba@gmail.com>
2025-01-12 00:39:43 +0200

    edit page load for darkmode

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,18 @@
 import type { Metadata } from "next";
 import { Outfit } from "next/font/google";
 import { Toaster } from "@/components/ui/toaster";
-`import "./globals.css";`;
+import "./globals.css";
 
 const outfit = Outfit({
   subsets: ["latin"],
   variable: "--font-outfit",
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
 });
+
 export const metadata: Metadata = {
-  title: "Eslam Adel Portofolio",
+  title: "Eslam Adel Portfolio",
   description:
-    "I am a creative and detail-oriented UX/UI developer passionate about designing intuitive, user-centered digital experiences. proficient in the latest design ...",
+    "I am a creative and detail-oriented UX/UI developer passionate about designing intuitive, user-centered digital experiences. Proficient in the latest design...",
 };
 
 export default function RootLayout({
@@ -22,11 +23,29 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className="flex flex-col w-full overflow-x-hidden dark"
+      className="flex flex-col w-full overflow-x-hidden"
       tabIndex={-1}
     >
+      <head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function() {
+                const storedTheme = localStorage.getItem("theme");
+                const systemTheme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
+                const theme = storedTheme || systemTheme;
+                if (theme === "dark") {
+                  document.documentElement.classList.add("dark");
+                } else {
+                  document.documentElement.classList.remove("dark");
+                }
+              })();
+            `,
+          }}
+        />
+      </head>
       <body className={`${outfit.variable} relative overflow-x-clip`}>
-        <main className="">{children}</main>
+        <main>{children}</main>
         <Toaster />
       </body>
     </html>
