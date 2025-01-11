45f7602b78ee026e3ed885b42ef7807a31ae3cfa
eslaamadel <eslaamadelwahba@gmail.com>
2025-01-11 21:11:24 +0200

    resovle toogleSwitch

--- app/components/ToogleSwitch.tsx
+++ app/components/ToogleSwitch.tsx
@@ -5,19 +5,24 @@ import { FaMoon } from "react-icons/fa";
 
 const ThemeToggle = () => {
   const [darkmode, setDarkMode] = useState(() => {
-    // Initialize darkmode based on localStorage value
-    const storedTheme = localStorage.getItem("theme");
-    return storedTheme === "dark"; // If localStorage has dark mode, set to true
+    // Check if the code is running in the browser
+    if (typeof window !== "undefined") {
+      const storedTheme = localStorage.getItem("theme");
+      return storedTheme === "dark"; // If localStorage has dark mode, set to true
+    }
+    return true; // Default to dark mode if not in the browser
   });
 
   useEffect(() => {
-    // Update the document class and localStorage based on darkmode state
-    if (darkmode) {
-      document.documentElement.classList.add("dark");
-      localStorage.setItem("theme", "dark");
-    } else {
-      document.documentElement.classList.remove("dark");
-      localStorage.setItem("theme", "light");
+    // Ensure this runs only on the client-side
+    if (typeof window !== "undefined") {
+      if (darkmode) {
+        document.documentElement.classList.add("dark");
+        localStorage.setItem("theme", "dark");
+      } else {
+        document.documentElement.classList.remove("dark");
+        localStorage.setItem("theme", "light");
+      }
     }
   }, [darkmode]);
 
