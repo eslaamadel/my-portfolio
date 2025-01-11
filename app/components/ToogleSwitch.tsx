782e58341a2a8b23b13331a556433ba58069e928
eslaamadel <eslaamadelwahba@gmail.com>
2025-01-11 21:36:33 +0200

    resolve toogle component

--- app/components/ToogleSwitch.tsx
+++ app/components/ToogleSwitch.tsx
@@ -1,29 +1,33 @@
-"use client";
 import { useState, useEffect } from "react";
 import { Toggle } from "@/components/ui/toggle";
 import { MdSunny } from "react-icons/md";
 import { FaMoon } from "react-icons/fa";
 
 const ThemeToggle = () => {
+  // Initialize darkmode state only on the client-side
   const [darkmode, setDarkMode] = useState(() => {
-    // Initialize darkmode based on localStorage value
-    const storedTheme = localStorage.getItem("theme");
-    return storedTheme === "dark"; // If localStorage has dark mode, set to true
+    if (typeof window !== "undefined") {
+      // Check if localStorage is available in the browser
+      const storedTheme = localStorage.getItem("theme");
+      return storedTheme === "dark"; // If localStorage has dark mode, set to true
+    }
+    return true; // Default to dark mode if not in the browser
   });
 
   useEffect(() => {
-    // Update the document class and localStorage based on darkmode state
-    if (darkmode) {
-      document.documentElement.classList.add("dark");
-      localStorage.setItem("theme", "dark");
-    } else {
-      document.documentElement.classList.remove("dark");
-      localStorage.setItem("theme", "light");
+    if (typeof window !== "undefined") {
+      // Ensure this runs only on the client-side
+      if (darkmode) {
+        document.documentElement.classList.add("dark");
+        localStorage.setItem("theme", "dark");
+      } else {
+        document.documentElement.classList.remove("dark");
+        localStorage.setItem("theme", "light");
+      }
     }
   }, [darkmode]);
-
   return (
-    <div>
+    <div className="flex">
       <Toggle aria-label="Toggle theme" onClick={() => setDarkMode(!darkmode)}>
         <FaMoon className="dark:hidden block" />
         <MdSunny className="dark:block hidden" />
